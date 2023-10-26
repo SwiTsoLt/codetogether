12b602d46c2575d27b9f60041e4cdb26715eba3f
unknown <8268363@gmail.com>
2023-10-26 20:41:48 +0300

    add eslint; remove url from script.js; update iceServers

--- src/script.js
+++ src/script.js
@@ -1,127 +1,115 @@
-const url = 'ws://localhost:3000'
-const socket = io();
+// eslint-disable-next-line no-undef
+const socket = io()
 
-const roomIdElem = document.getElementById('roomId');
-const createButton = document.getElementById('createButton');
-const joinButton = document.getElementById('joinButton');
-const copyButton = document.getElementById('copyButton');
-const audioElem = document.querySelector('audio');
+const roomIdElem = document.getElementById('roomId')
+const createButton = document.getElementById('createButton')
+const joinButton = document.getElementById('joinButton')
+const copyButton = document.getElementById('copyButton')
+const audioElem = document.querySelector('audio')
 
-const ownerText = document.getElementById('owner');
-const joinerText = document.getElementById('joiner');
+const ownerText = document.getElementById('owner')
+const joinerText = document.getElementById('joiner')
 
 let id
 let isConnected = false
 
 const iceServers = [
-    {
-        urls: [
-            "stun.l.google.com:19302",
-            "stun1.l.google.com:19302",
-            "stun2.l.google.com:19302",
-            "stun3.l.google.com:19302",
-            "stun4.l.google.com:19302",
-            "stun.ekiga.net",
-            "stun.ideasip.com",
-            "stun.rixtelecom.se",
-            "stun.schlund.de",
-            "stun.stunprotocol.org:3478",
-            "stun.voiparound.com",
-            "stun.voipbuster.com",
-            "stun.voipstunt.com",
-            "stun.voxgratia.org"
-        ]
-    },
-    {
-        urls: "turn:relay.backups.cz",
-        credential: "webrtc",
-        username: "webrtc"
-    },
-    {
-        urls: "turn:relay.backups.cz?transport=tcp",
-        credential: "webrtc",
-        username: "webrtc"
-    },
-];
+  {
+    urls: [
+      {
+        username: undefined,
+        credential: undefined,
+        url: 'stun:stun.voiparound.com',
+        urls: ['stun:stun.voiparound.com']
+      },
+      {
+        username: undefined,
+        credential: undefined,
+        url: 'stun:stun.voxgratia.org',
+        urls: ['stun:stun.voxgratia.org']
+      }
+    ]
+  }
+]
 
-const pc = new RTCPeerConnection()
+const pc = new RTCPeerConnection({ iceServers })
 
 // Listen emits
 
 socket.on('createSuccess', ({ roomId }) => {
-    roomIdElem.innerText = roomId
-    id = roomId
-    roomIdElem.parentElement.classList.add('show')
+  roomIdElem.innerText = roomId
+  id = roomId
+  roomIdElem.parentElement.classList.add('show')
 })
 
 socket.on('joinSuccess', () => {
-    alert('Join success!');
-    isConnected = true
-    socket.emit('write', { text: ownerText.value })
+  alert('Join success!')
+  isConnected = true
+  socket.emit('write', { text: ownerText.value })
 })
 
 socket.on('write', ({ text }) => {
-    joinerText.innerText = text
+  joinerText.innerText = text
 })
 
 socket.on('message', ({ message }) => {
-    alert(message)
+  alert(message)
 })
 
 socket.on('leave', () => {
-    alert('User leave from room')
-    isConnected = false
+  alert('User leave from room')
+  isConnected = false
 })
 
 // WebRTC handle emits
 
 socket.on('startWebRtcConnection', () => [
-    startWebRtcConnection()
+  startWebRtcConnection()
 ])
 
 socket.on('description', async ({ description }) => {
-    if (pc.localDescription) {
+  if (pc.localDescription) {
+    await pc.setRemoteDescription(description)
+  } else {
+    navigator.mediaDevices.getUserMedia({ audio: { echoCancellation: true } })
+      .then(async stream => {
         await pc.setRemoteDescription(description)
-    } else {
-        navigator.mediaDevices.getUserMedia({ audio: { echoCancellation: true } })
-            .then(async stream => {
-                await pc.setRemoteDescription(description)
 
-                stream.getAudioTracks().forEach(track => pc.addTrack(track))
+        stream.getAudioTracks().forEach(track => pc.addTrack(track))
 
-                const localDescription = await pc.createAnswer()
-                pc.setLocalDescription(localDescription)
-                socket.emit('description', { description: localDescription })
-            })
-    }
+        const localDescription = await pc.createAnswer()
+        pc.setLocalDescription(localDescription)
+        socket.emit('description', { description: localDescription })
+      })
+  }
 })
 
 socket.on('icecandidate', async ({ candidate }) => {
-    if (pc.remoteDescription) {
-        await pc.addIceCandidate(candidate)
-    }
+  if (pc.remoteDescription) {
+    await pc.addIceCandidate(candidate)
+  }
 })
 
 // Handle buttons
 
-function createRoom() {
-    socket.emit('create')
+function createRoom () {
+  socket.emit('create')
 }
 
-function joinRoom() {
-    const roomId = prompt('Room id: ')
-    if (!roomId) return
-    socket.emit('join', { roomId })
+function joinRoom () {
+  const roomId = prompt('Room id: ')
+  if (!roomId) return
+  socket.emit('join', { roomId })
 }
 
-function copyRoomId() {
-    navigator.clipboard.writeText(id)
+function copyRoomId () {
+  navigator.clipboard.writeText(id)
 }
 
-function handleWrite() {
-    if (isConnected) {
-        socket.emit('write', { text: ownerText.value })
-    }
+function handleWrite () {
+  if (isConnected) {
+    socket.emit('write', { text: ownerText.value })
+  }
 }
 
 createButton.addEventListener('click', createRoom)
@@ -132,37 +120,37 @@ ownerText.addEventListener('input', handleWrite)
 // WebRtc
 
 pc.addEventListener('icecandidate', event => {
-    if (event.candidate) {
-        socket.emit('icecandidate', { candidate: event.candidate })
-    }
+  if (event.candidate) {
+    socket.emit('icecandidate', { candidate: event.candidate })
+  }
 })
 
 pc.addEventListener('datachannel', () => {
-    console.log('web rtc connect!');
+  console.log('web rtc connect!')
 })
 
 pc.addEventListener('track', event => {
-    const m = new MediaStream()
-    m.addTrack(event.track)
-    audioElem.srcObject = m
-    audioElem.play()
-    console.log('play');
+  const m = new MediaStream()
+  m.addTrack(event.track)
+  audioElem.srcObject = m
+  audioElem.play()
+  console.log('play')
 })
 
-function startWebRtcConnection() {
-    navigator.mediaDevices.getUserMedia({ audio: { echoCancellation: true } })
-        .then(async stream => {
-            const dataChannel = pc.createDataChannel('audioChannel')
+function startWebRtcConnection () {
+  navigator.mediaDevices.getUserMedia({ audio: { echoCancellation: true } })
+    .then(async stream => {
+      const dataChannel = pc.createDataChannel('audioChannel')
 
-            stream.getTracks().forEach((track) => pc.addTrack(track))
+      stream.getTracks().forEach((track) => pc.addTrack(track))
 
-            dataChannel.addEventListener('open', () => {
-                console.log('web rtc connect!');
-            })
+      dataChannel.addEventListener('open', () => {
+        console.log('web rtc connect!')
+      })
 
-            const description = await pc.createOffer()
-            await pc.setLocalDescription(description)
+      const description = await pc.createOffer()
+      await pc.setLocalDescription(description)
 
-            socket.emit('description', { description })
-        })
+      socket.emit('description', { description })
+    })
 }
