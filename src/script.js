74939cb095f17cd090dc842b0ee4352438097df3
unknown <8268363@gmail.com>
2023-10-26 21:50:07 +0300

    update iceServers

--- src/script.js
+++ src/script.js
@@ -23,23 +23,8 @@ const joinerText = document.getElementById('joiner')
 let id
 let isConnected = false
 
-const pc = new RTCPeerConnection({
-  iceServers: [
-    {
-      urls: ['stun:stun.l.google.com:19302']
-    },
-    {
-      urls: ['turn:192.158.29.39:3478?transport=udp'],
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808'
-    },
-    {
-      urls: ['turn:192.158.29.39:3478?transport=tcp'],
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808'
-    }
-  ]
-})
+// eslint-disable-next-line no-undef
+const pc = new RTCPeerConnection({ iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }] })
 
 // Listen emits
 
